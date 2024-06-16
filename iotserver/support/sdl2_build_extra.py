26068a50bacb12a73d3fb33ab8a8cebf2a9e81a8
Lucas Marques <marques.analista@gmail.com>
2024-06-16 00:39:06 -0300

    format source

--- iotserver/support/sdl2_build_extra.py
+++ iotserver/support/sdl2_build_extra.py
@@ -1,6 +1,6 @@
 Import("env", "projenv")
 
-for e in [ env, projenv ]:
+for e in [env, projenv]:
     # If compiler uses `-m32`, propagate it to linker.
     # Add via script, because `-Wl,-m32` does not work.
     if "-m32" in e['CCFLAGS']:
@@ -10,6 +10,7 @@ exec_name = "${BUILD_DIR}/${PROGNAME}${PROGSUFFIX}"
 
 # Override unused "upload" to execute compiled binary
 from SCons.Script import AlwaysBuild
+
 AlwaysBuild(env.Alias("upload", exec_name, exec_name))
 
 # Add custom target to explorer
