9e878349ab1dc220398007e1e1f18949b705de11
lmarqs <lucas.marques@medprevonline.com>
2024-07-07 18:18:05 -0300

    [main] use complex app

--- smartwatch/main.c
+++ smartwatch/main.c
@@ -16,19 +16,11 @@ int main(void)
 
   hal_setup();
 
-  lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
-  lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-  lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+  setup_ui(&guider_ui);
 
-  lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
-  lv_label_set_text(label, "Button");                     /*Set the labels text*/
-  lv_obj_center(label);
+  events_init(&guider_ui);
 
-  // setup_ui(&guider_ui);
-
-  // events_init(&guider_ui);
-
-  // custom_init(&guider_ui);
+  custom_init(&guider_ui);
 
   hal_loop();
 
