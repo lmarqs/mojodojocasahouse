9adf9447520f6c61a700d59a2ccc20bb54a16df8
lmarqs <lucas.marques@medprevonline.com>
2024-07-07 22:16:27 -0300

    [main] create helper scripts

---
+++ smartwatch/scripts/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Stopping and disabling smartwatch service..."
+
+sudo systemctl stop smartwatch.service
+
+sudo systemctl disable smartwatch.service
+
+echo "Removing smartwatch files..."
+
+cat install_manifest_smartwatch.txt
+
+cat install_manifest_smartwatch.txt | xargs sudo rm -rf
+
+echo "Reloading systemctl..."
+
+sudo systemctl daemon-reload
