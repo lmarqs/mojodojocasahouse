ff4598cd847dc1d27ec15b47cf706e1840d9fe38
lmarqs <lucas.marques@medprevonline.com>
2024-07-09 19:13:39 -0300

    [main] setup pin

--- sensors/irremote.py
+++ sensors/irremote.py
@@ -4,9 +4,9 @@ import pulseio
 import board
 import adafruit_irremote
 
-pulsein = pulseio.PulseIn(board.REMOTEIN, maxlen=120, idle_state=True)
-decoder = adafruit_irremote.GenericDecode()
+pulsein = pulseio.PulseIn(board.D27, maxlen=120, idle_state=True)
 
+decoder = adafruit_irremote.GenericDecode()
 
 while True:
     pulses = decoder.read_pulses(pulsein)
