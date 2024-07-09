a76e6ad6a30585a27253e6f4ae551901c8babc9d
lmarqs <lucas.marques@medprevonline.com>
2024-07-09 19:24:19 -0300

    [main] use non blocking code

--- sensors/irremote.py
+++ sensors/irremote.py
@@ -1,22 +1,29 @@
-# Circuit Playground Express Demo Code
-# Adjust the pulseio 'board.PIN' if using something else
-import pulseio
+import time
+
 import board
+import pulseio
+
 import adafruit_irremote
 
 pulsein = pulseio.PulseIn(board.D27, maxlen=120, idle_state=True)
+decoder = adafruit_irremote.NonblockingGenericDecode(pulsein)
 
-decoder = adafruit_irremote.GenericDecode()
+t0 = next_heartbeat = time.monotonic()
 
 while True:
-    pulses = decoder.read_pulses(pulsein)
-    print("Heard", len(pulses), "Pulses:", pulses)
-    try:
-        code = decoder.decode_bits(pulses)
-        print("Decoded:", code)
-    except adafruit_irremote.IRNECRepeatException:  # unusual short code!
-        print("NEC repeat!")
-    except adafruit_irremote.IRDecodeException as e:     # failed to decode
-        print("Failed to decode: ", e.args)
+    for message in decoder.read():
+        print(f"t={time.monotonic() - t0:.3} New Message")
+        print("Heard", len(message.pulses), "Pulses:", message.pulses)
+        if isinstance(message, adafruit_irremote.IRMessage):
+            print("Decoded:", message.code)
+        elif isinstance(message, adafruit_irremote.NECRepeatIRMessage):
+            print("NEC repeat!")
+        elif isinstance(message, adafruit_irremote.UnparseableIRMessage):
+            print("Failed to decode", message.reason)
+        print("----------------------------")
 
-    print("----------------------------")
+    # This heartbeat confirms that we are not blocked somewhere above.
+    t = time.monotonic()
+    if t > next_heartbeat:
+        print(f"t={time.monotonic() - t0:.3} Heartbeat")
+        next_heartbeat = t + 0.1
