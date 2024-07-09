b309dbc4ef56342c27f6ed536327cda6dd8ef3cb
lmarqs <lucas.marques@medprevonline.com>
2024-07-09 19:35:25 -0300

    [main] fix repeat

--- sensors/irremote.py
+++ sensors/irremote.py
@@ -14,10 +14,12 @@ while True:
     for message in decoder.read():
         # print(f"t={time.monotonic() - t0:.3} New Message")
         # print("Heard", len(message.pulses), "Pulses:", message.pulses)
-        if isinstance(message, adafruit_irremote.IRMessage) or isinstance(message, adafruit_irremote.NECRepeatIRMessage):
+        if isinstance(message, adafruit_irremote.IRMessage):
           if len(message.code) > 3:
             command = message.code[2]
             print("Command:", command)
+        elif isinstance(message, adafruit_irremote.NECRepeatIRMessage):
+            print("NEC repeat!")
         elif isinstance(message, adafruit_irremote.UnparseableIRMessage):
             print("Failed to decode", message.reason)
         print("----------------------------")
