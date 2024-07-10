a47489420ed5019f93ed47c03053146c3303e7c6
lmarqs <lucas.marques@medprevonline.com>
2024-07-10 00:08:04 -0300

    [main] fix sensors

--- sensors/main.py
+++ sensors/main.py
@@ -10,4 +10,9 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
 
     while True:
         time.sleep(1)
-        s.sendall("T:{:.0f}".format(dht.temperature))
+        # s.sendall("T:{:.0f}".format(dht.temperature))
+        try:
+            print("Temp: {:.1f} *C \t Humidity: {}%".format(dht.temperature, dht.humidity))
+        except RuntimeError as e:
+            # Reading doesn't always work! Just print error and we'll try again
+            print("Reading from DHT failure: ", e.args)
