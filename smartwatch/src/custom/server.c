545ba327d3c0102a883ff2ac52fd96d85c6dd2c0
lmarqs <lucas.marques@medprevonline.com>
2024-07-10 00:42:08 -0300

    [main] fix server

--- smartwatch/src/custom/server.c
+++ smartwatch/src/custom/server.c
@@ -9,6 +9,7 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include "server.h"
+#include "lvgl/lvgl.h"
 
 struct connection_listening_thread_args
 {
@@ -31,11 +32,13 @@ static void * listening_thread(void *raw_args)
     uint8_t buf[0xFF];
     size_t buf_len = sizeof(buf);
 
+    memset(buf, 0, buf_len);
+
     ssize_t data_len = 0;
 
     while ((data_len = recv(client_fd, buf, buf_len, 0)) > 0)
     {
-      (*args->callback)(buf, data_len);
+      (*(args->callback))(buf, data_len);
     }
   }
 }
@@ -46,7 +49,7 @@ int custom_server_create(struct sockaddr_un *server_addr, custom_server_callback
 
   if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
   {
-    printf("Error on socket() call \n");
+    LV_LOG_ERROR("Error on socket() call ");
 
     return -1;
   }
@@ -55,21 +58,21 @@ int custom_server_create(struct sockaddr_un *server_addr, custom_server_callback
 
   if (bind(server_fd, (struct sockaddr *)server_addr, strlen(server_addr->sun_path) + sizeof(server_addr->sun_family)))
   {
-    printf("Error on binding socket \n");
+    LV_LOG_ERROR("Error on binding socket ");
 
     return -1;
   }
 
   if (chmod(server_addr->sun_path, 0b111111111))
   {
-    printf("Error on chmod socket \n");
+    LV_LOG_ERROR("Error on chmod socket ");
 
     return -1;
   }
 
   if (listen(server_fd, 5))
   {
-    printf("Error on listen call \n");
+    LV_LOG_ERROR("Error on listen call ");
 
     return -1;
   }
@@ -82,7 +85,7 @@ int custom_server_create(struct sockaddr_un *server_addr, custom_server_callback
 
   if (pthread_create(&listening_tid, NULL, listening_thread, &listening_args) != 0)
   {
-    printf("Failed to create thread\n");
+    LV_LOG_ERROR("Failed to create thread");
   }
 
   return 0;
