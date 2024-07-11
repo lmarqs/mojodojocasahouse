c06d722ecdfa25739e383ca72cf59a863266ef91
lmarqs <lucas.marques@medprevonline.com>
2024-07-11 01:10:00 -0300

    [main] finally

--- smartwatch/src/custom/server.c
+++ smartwatch/src/custom/server.c
@@ -11,7 +11,7 @@
 #include "server.h"
 #include "lvgl/lvgl.h"
 
-static struct connection_listening_thread_args
+struct connection_listening_thread_args
 {
   custom_server_callback_t callback;
   int server_fd;
@@ -20,6 +20,8 @@ static struct connection_listening_thread_args
 static void *listening_thread(void *raw_args)
 {
   struct connection_listening_thread_args *args = (struct connection_listening_thread_args *)raw_args;
+
+  printf("args: %d %d %d\n", args, args->callback, args->server_fd);
 
   while (true)
   {
@@ -50,7 +52,6 @@ int custom_server_create(struct sockaddr_un *server_addr, custom_server_callback
   if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
   {
     LV_LOG_ERROR("Error on socket() call");
-
     return -1;
   }
 
@@ -59,38 +60,35 @@ int custom_server_create(struct sockaddr_un *server_addr, custom_server_callback
   if (bind(server_fd, (struct sockaddr *)server_addr, strlen(server_addr->sun_path) + sizeof(server_addr->sun_family)))
   {
     LV_LOG_ERROR("Error on binding socket");
-
     return -1;
   }
 
   if (chmod(server_addr->sun_path, 0b111111111))
   {
     LV_LOG_ERROR("Error on chmod socket");
-
     return -1;
   }
 
   if (listen(server_fd, 5))
   {
     LV_LOG_ERROR("Error on listen call");
-
     return -1;
   }
 
   struct connection_listening_thread_args *listening_targs;
 
-  if ((listening_targs = malloc(sizeof(struct connection_listening_thread_args))) == NULL)
+  if ((listening_targs = (struct connection_listening_thread_args *)malloc(sizeof(struct connection_listening_thread_args))) == NULL)
   {
     LV_LOG_ERROR("Failed to allocate memory");
     return -1;
   }
 
-  listening_targs->server_fd = server_fd;
   listening_targs->callback = callback;
+  listening_targs->server_fd = server_fd;
 
   pthread_t listening_tid = 0x0;
 
-  if (pthread_create(&listening_tid, NULL, listening_thread, &listening_targs) != 0)
+  if (pthread_create(&listening_tid, NULL, listening_thread, listening_targs) != 0)
   {
     LV_LOG_ERROR("Failed to create thread");
     return -1;
