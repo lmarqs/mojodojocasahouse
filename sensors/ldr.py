c4200dc0914d1e68d2957c2f5e3babb3418004cf
lmarqs <lucas.marques@medprevonline.com>
2024-07-19 00:33:11 -0300

    [main] add switch

--- sensors/ldr.py
+++ sensors/ldr.py
@@ -2,9 +2,13 @@ import board
 import digitalio
 import time
 
-sensor = digitalio.DigitalInOut(board.D25)
-sensor.direction = digitalio.Direction.INPUT
+# sensor = digitalio.DigitalInOut(board.D25)
+# sensor.direction = digitalio.Direction.INPUT
+
+switch = digitalio.DigitalInOut(board.D26)
+switch.direction = digitalio.Direction.OUTPUT
 
 while True:
     time.sleep(1)
-    print("T:{:.0f}".format(sensor.value))
+    switch.value = not switch.value
+    # print("T:{:.0f}".format(sensor.value))
