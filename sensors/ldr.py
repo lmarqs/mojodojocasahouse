54b91b3856ec8efde0834e7a2222912f5e9882a1
lmarqs <lucas.marques@medprevonline.com>
2024-07-19 00:41:37 -0300

    [main] refact ldr

--- sensors/ldr.py
+++ sensors/ldr.py
@@ -2,13 +2,9 @@ import board
 import digitalio
 import time
 
-# sensor = digitalio.DigitalInOut(board.D25)
-# sensor.direction = digitalio.Direction.INPUT
-
-switch = digitalio.DigitalInOut(board.D26)
-switch.direction = digitalio.Direction.OUTPUT
+sensor = digitalio.DigitalInOut(board.D25)
+sensor.direction = digitalio.Direction.INPUT
 
 while True:
     time.sleep(1)
-    switch.value = not switch.value
-    # print("T:{:.0f}".format(sensor.value))
+    print(sensor.value)
