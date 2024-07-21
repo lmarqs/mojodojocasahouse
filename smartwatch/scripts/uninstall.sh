4507df35bb979d550f97fb73169bda4625b6c344
lmarqs <lucas.marques@medprevonline.com>
2024-07-21 19:18:56 -0300

    [main] install system config files

--- smartwatch/scripts/uninstall.sh
+++ smartwatch/scripts/uninstall.sh
@@ -5,13 +5,3 @@ echo "Stopping and disabling smartwatch service..."
 sudo systemctl stop smartwatch.service
 
 sudo systemctl disable smartwatch.service
-
-echo "Removing smartwatch files..."
-
-cat install_manifest_smartwatch.txt
-
-cat install_manifest_smartwatch.txt | xargs sudo rm -rf
-
-echo "\nReloading systemctl..."
-
-sudo systemctl daemon-reload
