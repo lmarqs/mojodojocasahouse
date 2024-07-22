a8937d75428407cd1c58f8376901b5309bd21671
lmarqs <lucas.marques@medprevonline.com>
2024-07-21 23:25:52 -0300

    [main] log steps

--- sensors/scripts/prepare.sh
+++ sensors/scripts/prepare.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-echo "Preparing venv for sensors..."
+echo "Preparing env for sensors..."
+
+echo "Installing libs..."
+
+sudo apt-get update
 
 sudo apt-get install -y i2c-tools libgpiod-dev python3-libgpiod
+
+echo "setting up interfaces..."
 
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
@@ -11,7 +17,12 @@ sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint disable_raspi_config_at_boot 0
 
+echo "Creating virtual env..."
+
 sudo python3 -m venv /opt/mdch/venv --system-site-packages
+
+
+echo "Installing python libs..."
 
 sudo /opt/mdch/venv/bin/pip3 install -r requirements.txt
 
